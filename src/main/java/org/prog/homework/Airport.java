8c5deeca59e090a3cbcf93e1fefd39feb83e8985
Max1996123 <trefil1234@gmail.com>
2023-02-05 17:15:31 +0200

    HomeWork commit

--- src/main/java/org/prog/homework/Airport.java
+++ src/main/java/org/prog/homework/Airport.java
@@ -1,19 +1,53 @@
 package org.prog.homework;
 
+
+
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Airport {
 
-    public final List<Plane> planes = new ArrayList<>();
+
+    public final List<Plane> planes = new ArrayList<>() {
+
+
+    };
 
     //if no plane found - throw exception
-    public Plane getPlaneByFlightIdAndDestination() {
+    public Plane getPlaneByFlightIdAndDestination(String destination, String flightNumber) {
+
+        try {
+            String planeByFlightIdAndDestination = planes.stream()
+                    .filter(c -> c.destination.equals(destination))
+                    .filter(c -> c.flightNumber.equals(flightNumber))
+                    .map(c -> c.flightNumber + " " + destination)
+                    .findAny().get();
+
+            System.out.println(planeByFlightIdAndDestination);
+        } catch (Exception e) {
+            System.out.println("failed to find such aircraft");
+        }
+
+
         return null;
+    }
+    //if no plane found - throw exception
+    public void planeByDestination( String destination) {
+
+        List<Plane> planeByDestination = planes.stream()
+                .filter(s -> s.destination.contains(destination))
+                .collect(Collectors.toList());
+        if ( planeByDestination.isEmpty()) {
+            System.out.println("failed to find such aircraft");
+        }
+        else{
+
+            for (Plane i : planeByDestination) {
+                System.out.println(i.destination + " " + i.flightNumber);
+            }
+        }
     }
 
-    //if no plane found - throw exception
-    public List<Plane> planeByDestination() {
-        return null;
-    }
 }
--- src/main/java/org/prog/homework/HomeworkMain.java
+++ src/main/java/org/prog/homework/HomeworkMain.java
@@ -3,6 +3,36 @@ package org.prog.homework;
 public class HomeworkMain {
 
     public static void main(String... args) {
+        //Kyiv chernihiv chop
+        //one two three four five
+        Plane planeOne = new Plane();
+        planeOne.destination = "Kyiv";
+        planeOne.flightNumber = "00AA999";
+
+        Plane planeTwo = new Plane();
+        planeTwo.destination = "Chernihiv";
+        planeTwo.flightNumber ="11BB876" ;
+
+        Plane planeThree = new Plane();
+        planeThree.destination = "Chop";
+        planeThree.flightNumber = "22FF433";
+
+        Plane planeFour = new Plane();
+        planeFour.destination = "Kyiv";
+        planeFour.flightNumber = "11GG343";
+
+        Airport airport = new Airport();
+        airport.planes.add(planeOne);
+        airport.planes.add(planeTwo);
+        airport.planes.add(planeThree);
+        airport.planes.add(planeFour);
+        airport.getPlaneByFlightIdAndDestination("Kyiv","11GG343");
+        airport.getPlaneByFlightIdAndDestination("Kyiv","1G343");
+        airport.planeByDestination("Kyiv");
+        airport.planeByDestination("Uruguay");
+        airport.planeByDestination("Chop");
+
+
 
     }
 }
