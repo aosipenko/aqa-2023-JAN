c5e26eb28a34f392a47fee651bfb0dc5035403a6
Alexy <aoskiev@gmail.com>
2023-02-23 19:25:12 +0100

    maven surefire + SQL Read

--- src/test/java/org/prog/steps/RestSteps.java
+++ src/test/java/org/prog/steps/RestSteps.java
@@ -1,36 +1,41 @@
 package org.prog.steps;
 
+import static org.hamcrest.Matchers.hasItems;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
+import org.junit.Assert;
 import org.prog.dto.ResultsDto;
 import org.prog.dto.UserDto;
 import org.prog.util.DataHolder;
 
-import static org.hamcrest.Matchers.hasItems;
-
 public class RestSteps {
 
-    private final static String REQUEST =
-            "https://randomuser.me/api/?inc=gender,name,nat&noinfo&results=20";
+  private final static String REQUEST =
+      "https://randomuser.me/api/?inc=gender,name,nat&noinfo&results=20";
 
-    @Given("I generate a random {string}")
-    public void generateRandomUser(String alias) {
-        DataHolder.getInstance().put(alias, getUserName());
-    }
+  @Given("I generate a random {string}")
+  public void generateRandomUser(String alias) {
+    DataHolder.getInstance().put(alias, getUserName());
+  }
 
-    private UserDto getUserName() {
-        Response response = RestAssured.get(REQUEST);
-        ValidatableResponse validatableResponse = response.then();
+  @Then("Person {string} has gender {string}")
+  public void guessGender(String alias, String gender) {
+    UserDto userDto = (UserDto) DataHolder.getInstance().get(alias);
+    Assert.assertEquals(gender, userDto.getGender());
+  }
 
-        validatableResponse.assertThat()
-                .statusCode(200)
-                .body("results.gender", hasItems("male", "female"));
+  private UserDto getUserName() {
+    Response response = RestAssured.get(REQUEST);
+    ValidatableResponse validatableResponse = response.then();
 
-        ResultsDto resultsDto = response.as(ResultsDto.class);
-        return resultsDto.getResults().stream()
-                .filter(userDto -> userDto.getGender().equals("male"))
-                .findFirst().get();
-    }
+    validatableResponse.assertThat()
+        .statusCode(200)
+        .body("results.gender", hasItems("male", "female"));
+
+    ResultsDto resultsDto = response.as(ResultsDto.class);
+    return resultsDto.getResults().stream().findFirst().get();
+  }
 }
--- src/test/java/org/prog/steps/SQLSteps.java
+++ src/test/java/org/prog/steps/SQLSteps.java
@@ -1,65 +1,92 @@
 package org.prog.steps;
 
 import io.cucumber.java.en.Given;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Optional;
 import org.junit.Assert;
+import org.prog.dto.NameDto;
 import org.prog.dto.UserDto;
 import org.prog.util.DataHolder;
 
-import java.sql.*;
-import java.util.Optional;
-
 public class SQLSteps {
 
-    private final static String FILTER_BY_PARAMETER =
-            "select FirstName, LastName from Persons WHERE %s = '%s'";
+  private final static String FILTER_BY_PARAMETER =
+      "select FirstName, LastName from Persons WHERE %s = '%s'";
 
-    private final static String NEW_USER_SQL =
-            "insert into Persons (LastName, FirstName, Title, Gender)" +
-                    " VALUES ('%s', '%s', '%s', '%s')";
+  private final static String FILTER_BY_PARAMETER_ONE_RECORD =
+      "select FirstName, LastName from Persons WHERE %s = '%s' LIMIT 0,1";
 
-    @Given("I save person {string} to DB")
-    public void savePersonToDB(String alias) {
-        try {
-            UserDto userDto = (UserDto) DataHolder.getInstance().get(alias);
+  private final static String NEW_USER_SQL =
+      "insert into Persons (LastName, FirstName, Title, Gender)" +
+          " VALUES ('%s', '%s', '%s', '%s')";
 
-            String query = String.format(NEW_USER_SQL, userDto.getName().getLast(), userDto.getName().getFirst(),
-                    userDto.getName().getTitle(), userDto.getGender());
-            executeQuery(query, false);
-        } catch (SQLException e) {
-            System.err.println("Failed to create user due to SQL Exception!");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+  @Given("I save person {string} to DB")
+  public void savePersonToDB(String alias) {
+    try {
+      UserDto userDto = (UserDto) DataHolder.getInstance().get(alias);
+
+      String query = String.format(NEW_USER_SQL, userDto.getName().getLast(), userDto.getName().getFirst(),
+          userDto.getName().getTitle(), userDto.getGender());
+      executeQuery(query, false);
+    } catch (SQLException e) {
+      System.err.println("Failed to create user due to SQL Exception!");
+    } catch (ClassNotFoundException e) {
+      throw new RuntimeException(e);
     }
+  }
 
-    @Given("I print all users in DB with {string} = {string}")
-    public void setupConnection(String parameter, String value) throws ClassNotFoundException, SQLException {
-        String query = String.format(FILTER_BY_PARAMETER, parameter, value);
-        Optional<ResultSet> sqlResults = executeQuery(query, true);
-        if (sqlResults.isPresent()) {
-            while (sqlResults.get().next()) {
-                String name = sqlResults.get().getString(1) + " " + sqlResults.get().getString(2);
-                System.out.println(name);
-            }
-        } else {
-            Assert.fail("SQL Execution failed!");
-        }
+  @Given("I print all users in DB with {string} = {string}")
+  public void setupConnection(String parameter, String value) throws ClassNotFoundException, SQLException {
+    String query = String.format(FILTER_BY_PARAMETER, parameter, value);
+    Optional<ResultSet> sqlResults = executeQuery(query, true);
+    if (sqlResults.isPresent()) {
+      while (sqlResults.get().next()) {
+        String name = sqlResults.get().getString(1) + " " + sqlResults.get().getString(2);
+        System.out.println(name);
+      }
+    } else {
+      Assert.fail("SQL Execution failed!");
     }
+  }
 
-    private Optional<ResultSet> executeQuery(String query, boolean hasResponse)
-            throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
+  private Optional<ResultSet> executeQuery(String query, boolean hasResponse)
+      throws ClassNotFoundException, SQLException {
+    Class.forName("com.mysql.cj.jdbc.Driver");
 
-        Connection con =
-                DriverManager.getConnection("jdbc:mysql://localhost:3306/db",
-                        "user", "password");
+    Connection con =
+        DriverManager.getConnection("jdbc:mysql://localhost:3306/db",
+            "user", "password");
 
-        Statement stmt = con.createStatement();
-        if (hasResponse) {
-            return Optional.of(stmt.executeQuery(query));
-        } else {
-            stmt.execute(query);
-            return Optional.empty();
-        }
+    Statement stmt = con.createStatement();
+    if (hasResponse) {
+      return Optional.of(stmt.executeQuery(query));
+    } else {
+      stmt.execute(query);
+      return Optional.empty();
     }
+  }
+
+  @Given("I retrieve person with {string} = {string} as {string}")
+  public void iRetrievePersonFromDB(String dbColumnName, String dbColumnValue, String alias)
+      throws SQLException, ClassNotFoundException {
+    String query = String.format(FILTER_BY_PARAMETER_ONE_RECORD, dbColumnName, dbColumnValue);
+    Optional<ResultSet> sqlResults = executeQuery(query, true);
+
+    UserDto userDto = new UserDto();
+    userDto.setName(new NameDto());
+
+    if (sqlResults.isPresent()) {
+      while (sqlResults.get().next()) {
+        userDto.getName().setFirst(sqlResults.get().getString(1));
+        userDto.getName().setLast(sqlResults.get().getString(2));
+      }
+      DataHolder.getInstance().put(alias, userDto);
+    } else {
+      Assert.fail("SQL Execution failed!");
+    }
+  }
 }
