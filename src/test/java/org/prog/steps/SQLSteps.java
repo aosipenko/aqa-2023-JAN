f6950b6c59764de5864087e8c6222f12adcb74dd
oo965787 <oleksii.osypenko@telenor.se>
2023-03-02 16:37:19 +0100

    Session 10 (pre-session): jenkins docker setup

--- src/test/java/org/prog/steps/SQLSteps.java
+++ src/test/java/org/prog/steps/SQLSteps.java
@@ -1,92 +1,97 @@
 package org.prog.steps;
 
 import io.cucumber.java.en.Given;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Optional;
+import lombok.SneakyThrows;
 import org.junit.Assert;
 import org.prog.dto.NameDto;
 import org.prog.dto.UserDto;
 import org.prog.util.DataHolder;
 
+import java.net.InetAddress;
+import java.sql.*;
+import java.util.Optional;
+
 public class SQLSteps {
 
-  private final static String FILTER_BY_PARAMETER =
-      "select FirstName, LastName from Persons WHERE %s = '%s'";
+    private final static String FILTER_BY_PARAMETER =
+            "select FirstName, LastName from Persons WHERE %s = '%s'";
 
-  private final static String FILTER_BY_PARAMETER_ONE_RECORD =
-      "select FirstName, LastName from Persons WHERE %s = '%s' LIMIT 0,1";
+    private final static String FILTER_BY_PARAMETER_ONE_RECORD =
+            "select FirstName, LastName from Persons WHERE %s = '%s' LIMIT 0,1";
 
-  private final static String NEW_USER_SQL =
-      "insert into Persons (LastName, FirstName, Title, Gender)" +
-          " VALUES ('%s', '%s', '%s', '%s')";
+    private final static String NEW_USER_SQL =
+            "insert into Persons (LastName, FirstName, Title, Gender)" +
+                    " VALUES ('%s', '%s', '%s', '%s')";
 
-  @Given("I save person {string} to DB")
-  public void savePersonToDB(String alias) {
-    try {
-      UserDto userDto = (UserDto) DataHolder.getInstance().get(alias);
+    @Given("I save person {string} to DB")
+    public void savePersonToDB(String alias) {
+        try {
+            UserDto userDto = (UserDto) DataHolder.getInstance().get(alias);
 
-      String query = String.format(NEW_USER_SQL, userDto.getName().getLast(), userDto.getName().getFirst(),
-          userDto.getName().getTitle(), userDto.getGender());
-      executeQuery(query, false);
-    } catch (SQLException e) {
-      System.err.println("Failed to create user due to SQL Exception!");
-    } catch (ClassNotFoundException e) {
-      throw new RuntimeException(e);
+            String query = String.format(NEW_USER_SQL, userDto.getName().getLast(), userDto.getName().getFirst(),
+                    userDto.getName().getTitle(), userDto.getGender());
+            executeQuery(query, false);
+        } catch (SQLException e) {
+            System.err.println("Failed to create user due to SQL Exception!");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
-  }
 
-  @Given("I print all users in DB with {string} = {string}")
-  public void setupConnection(String parameter, String value) throws ClassNotFoundException, SQLException {
-    String query = String.format(FILTER_BY_PARAMETER, parameter, value);
-    Optional<ResultSet> sqlResults = executeQuery(query, true);
-    if (sqlResults.isPresent()) {
-      while (sqlResults.get().next()) {
-        String name = sqlResults.get().getString(1) + " " + sqlResults.get().getString(2);
-        System.out.println(name);
-      }
-    } else {
-      Assert.fail("SQL Execution failed!");
+    @Given("I print all users in DB with {string} = {string}")
+    public void setupConnection(String parameter, String value) throws ClassNotFoundException, SQLException {
+        String query = String.format(FILTER_BY_PARAMETER, parameter, value);
+        Optional<ResultSet> sqlResults = executeQuery(query, true);
+        if (sqlResults.isPresent()) {
+            while (sqlResults.get().next()) {
+                String name = sqlResults.get().getString(1) + " " + sqlResults.get().getString(2);
+                System.out.println(name);
+            }
+        } else {
+            Assert.fail("SQL Execution failed!");
+        }
     }
-  }
 
-  private Optional<ResultSet> executeQuery(String query, boolean hasResponse)
-      throws ClassNotFoundException, SQLException {
-    Class.forName("com.mysql.cj.jdbc.Driver");
+    @SneakyThrows
+    private Optional<ResultSet> executeQuery(String query, boolean hasResponse)
+            throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con;
 
-    Connection con =
-        DriverManager.getConnection("jdbc:mysql://localhost:3306/db",
-            "user", "password");
+        String hostName = InetAddress.getLocalHost().getHostName();
+        if ("TSELSE3871808".equals(hostName)) {
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db", "user", "password");
+        } else {
+            con = DriverManager.getConnection("jdbc:mysql://mysql:3306/db", "user", "password");
+        }
 
-    Statement stmt = con.createStatement();
-    if (hasResponse) {
-      return Optional.of(stmt.executeQuery(query));
-    } else {
-      stmt.execute(query);
-      return Optional.empty();
+
+        Statement stmt = con.createStatement();
+        if (hasResponse) {
+            return Optional.of(stmt.executeQuery(query));
+        } else {
+            stmt.execute(query);
+            return Optional.empty();
+        }
     }
-  }
 
-  @Given("I retrieve person with {string} = {string} as {string}")
-  public void iRetrievePersonFromDB(String dbColumnName, String dbColumnValue, String alias)
-      throws SQLException, ClassNotFoundException {
-    String query = String.format(FILTER_BY_PARAMETER_ONE_RECORD, dbColumnName, dbColumnValue);
-    Optional<ResultSet> sqlResults = executeQuery(query, true);
+    @Given("I retrieve person with {string} = {string} as {string}")
+    public void iRetrievePersonFromDB(String dbColumnName, String dbColumnValue, String alias)
+            throws SQLException, ClassNotFoundException {
+        String query = String.format(FILTER_BY_PARAMETER_ONE_RECORD, dbColumnName, dbColumnValue);
+        Optional<ResultSet> sqlResults = executeQuery(query, true);
 
-    UserDto userDto = new UserDto();
-    userDto.setName(new NameDto());
+        UserDto userDto = new UserDto();
+        userDto.setName(new NameDto());
 
-    if (sqlResults.isPresent()) {
-      while (sqlResults.get().next()) {
-        userDto.getName().setFirst(sqlResults.get().getString(1));
-        userDto.getName().setLast(sqlResults.get().getString(2));
-      }
-      DataHolder.getInstance().put(alias, userDto);
-    } else {
-      Assert.fail("SQL Execution failed!");
+        if (sqlResults.isPresent()) {
+            while (sqlResults.get().next()) {
+                userDto.getName().setFirst(sqlResults.get().getString(1));
+                userDto.getName().setLast(sqlResults.get().getString(2));
+            }
+            DataHolder.getInstance().put(alias, userDto);
+        } else {
+            Assert.fail("SQL Execution failed!");
+        }
     }
-  }
 }
